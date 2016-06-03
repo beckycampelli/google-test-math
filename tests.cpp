7efdf567ab456dfd4f17da8ee5d7009e1b898ff5
beckycampelli <rlcampelli@gmail.com>
2016-06-03 10:25:51 -0400

    Add files via upload
    These are the files needed to run an adapted "Math," provided that you have google test installed. They are in need of revision, as math_ex_test.png illustrates.
    For more info on google test, see tree.taiga.io/project/darrieng-command-and-data-handling/wiki/google-test

---
+++ tests.cpp
@@ -0,0 +1,44 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "Multiply.h"
+
+class AdditionTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test
+    // (right before the destructor).
+  }
+};
+
+class MultiplyTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+};
+
+TEST_F(AdditionTest,add){
+    const int x = 4;
+    const int y = 5;
+    Addition addition;
+    EXPECT_EQ(9,addition.add(x,y));
+    EXPECT_EQ(5,addition.add(2,3));
+}
+
+TEST_F(MultiplyTest,mult){
+    const int x = 4;
+    const int y = 5;
+    Multiply multiply;
+    EXPECT_EQ(20,multiply.mult(x,y));
+    EXPECT_EQ(6,multiply.mult(2,3));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
